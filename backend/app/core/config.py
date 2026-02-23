e11b7be700f61fa0d37c11b2998704b956ca6269
riaz-kp <riazkpard@gmail.com>
2026-02-24 00:09:54 +0530

    fix: added hostname in backend for cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Landshaft Plants Database"
-    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173","http://localhost:4173", "http://localhost:3000"]
+    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173","https://plants-database.vercel.app","http://localhost:4173", "http://localhost:3000"]
     
     # Database
     POSTGRES_SERVER: str = "localhost"
